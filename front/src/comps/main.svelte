cd98a1498d962a6f208a51973bffe1c33eabef1b
lazarevic-konstantin <127831884+klazarevic-ftn@users.noreply.github.com>
2024-12-09 12:16:24 +0100

    meh

--- front/src/comps/main.svelte
+++ front/src/comps/main.svelte
@@ -2,30 +2,49 @@
 <script>
     import Segment from './segment.svelte';
     let options = ['name', 'desc', 'desc from file', '# of posts', 'avg likes'];
+    let query = [
+        [
+            {'name':'name1','query':undefined, 'from':undefined,'to':undefined},
+            {'name':'name2','query':undefined, 'from':undefined,'to':undefined},
+            {'name':'name3','query':undefined, 'from':undefined,'to':undefined}
+        ],
+        [
+            {'name':'name4','query':undefined, 'from':undefined,'to':undefined},
+            {'name':'name5','query':undefined, 'from':undefined,'to':undefined},
+            {'name':'name6','query':undefined, 'from':undefined,'to':undefined}
+        ]
+    ];
+
+    function remove(event) {
+        let { first, second } = event.detail;
+        console.log(event.detail);
+        query[first].splice(second, 1);
+        query = query;
+    }
+
+    function addOr() {
+        query.push([{'name':'name4','query':undefined, 'from':undefined,'to':undefined}]);
+        query = query;
+    }
 </script>
 
-<div class="main column">
-    <div class="segment row">
-        <input>
-        <select>
-            {#each options as option}
-                <option>{option}</option>
+<div class="column">
+    {#each query as row}
+    <div class="row">
+            {#each row as segment}
+            <Segment on:remove={remove} content={segment} last={query.indexOf(row) === 0 && row.indexOf(segment) === 0} location={`${query.indexOf(row)}${row.indexOf(segment)}`}/>
+            {#if row.indexOf(segment) + 1 === row.length}
+                <button>+AND</button>
+            {:else if row.length > 1 && row.length < 5}
+                AND
+            {/if}
             {/each}
-        </select>
-        <button>+AND</button>
     </div>
-    <button>+OR</button>
+    {/each}
+    <button on:click={addOr}>+OR</button>
 </div>
 
 <style>
-    .main {
-
-    }
-
-    .segment {
-
-    }
-
     .row {
         display: flex;
         flex-flow: row nowrap;
--- front/src/comps/segment.svelte
+++ front/src/comps/segment.svelte
@@ -1,7 +1,23 @@
+<script>
+    import { createEventDispatcher } from 'svelte'
+    const dispatch = createEventDispatcher()
+
+    export let content;
+    export let last;
+    export let location;
+
+    function remove() {
+        let coors = location.split('').map(num => parseInt(num));
+        dispatch('remove', {
+           first: coors[0], second: coors[1]
+        });
+    }
+</script>
+
 <div class="segment">
-    <button>X</button>
+    <button disabled={last} on:click={remove}>X</button>
     <div>
-        <input>
+        Search <input value={content.name}> in
         <select><option>segment</option></select>
     </div>
 </div>
